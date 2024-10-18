36c3b310ac22eccf6a40e8aadecb58c74f385421
ALI JAN <133298273+AliJanGithub@users.noreply.github.com>
2024-10-19 03:15:40 +0500

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@
     
 // }
 const mongoose = require('mongoose');
-const connecttodb=mongoose.connect('mongodb+srv://alijan061333:U3q98KrLATMVBCAe@cluster0.5ly21.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+const connecttodb=mongoose.connect('mongodb+srv://alijan061333:ugtipMKiHCkrEVc9@cluster0.6j7uu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => {
     console.log('Connected to database!');
   })
